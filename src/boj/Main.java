b1a94231e3c42ba9b8b2208f3f64d662c33b469f
HI0516 <wangsun7@naver.com>
2023-03-28 22:50:58 +0900

    boj_Main 양식 수정

--- src/boj/Main.java
+++ src/boj/Main.java
@@ -1,21 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
-/*
- * https://www.acmicpc.net/problem/
- *
+/**
+ * @author 왕준영
+ * @see <a href = "https://www.acmicpc.net/problem/"></a>
+ * @category
+ * @Study
  */
 
 public class Main {
     static BufferedReader br;
+    static BufferedWriter bw;
     static StringTokenizer st;
-    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
-
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
     }
