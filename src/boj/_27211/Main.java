fa6f9c5a0254ea2e4ff24bf6a61b1c8a08375e08
왕준영 <wangsun7@naver.com>
2025-03-09 00:33:09 +0900

    boj_27211

---
+++ src/boj/_27211/Main.java
@@ -0,0 +1,108 @@
+package _27211;
+import java.io.*;
+import java.util.*;
+
+/**
+ * @link <a href="https://www.acmicpc.net/problem/27211"/>
+ */
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    Solution.createSolution()
+        .start();
+  }
+}
+
+class Solution {
+
+  int N,M;
+  int[][] board;
+
+  private void readInput() throws IOException {
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    board = new int[N][M];
+    for(int i=0; i<N; i++){
+      st = new StringTokenizer(br.readLine());
+      for(int j=0; j<M; j++){
+        board[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+  }
+
+  private static class Point{
+    int x,y;
+
+    public Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+  boolean[][] isVisited;
+  int count ;
+  private void solve() {
+    isVisited = new boolean[N][M];
+    count = 0;
+
+    for(int i=0; i<N; i++){
+      for(int j=0; j<M; j++){
+        if(isVisited[i][j]) continue;
+        if(board[i][j]==1) continue;
+        
+        count++;
+        bfsFrom(new Point(i,j));
+      }
+    }
+  }
+
+  int[] dx = {1,0,-1,0};
+  int[] dy = {0,1,0,-1};
+
+  private void bfsFrom(Point start){
+    Queue<Point> q = new ArrayDeque<>();
+
+    q.offer(start);
+    isVisited[start.x][start.y] = true;
+
+    while(!q.isEmpty()){
+      Point current = q.poll();
+
+      for(int i=0; i<4; i++){
+        int nx = (N + current.x + dx[i]) % N;
+        int ny = (M + current.y + dy[i]) % M;
+
+        if(isVisited[nx][ny]) continue;
+        if(board[nx][ny]==1) continue;
+
+        isVisited[nx][ny] = true;
+        q.offer(new Point(nx,ny));
+      }
+    }
+  }
+
+  private void writeOutput() throws IOException {
+    bw.write(Integer.toString(count));
+    bw.flush();
+  }
+
+  public void start() throws IOException {
+    readInput();
+    solve();
+    writeOutput();
+  }
+
+  private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  private StringTokenizer st;
+
+  private Solution() {
+  }
+
+  public static Solution createSolution() {
+    return new Solution();
+  }
+}
+
